e2c38f53b07f4cd01b99425b8642001065a44615
Jay Pal <contactjaypal@gmail.com>
2020-05-01 22:55:04 +0530

    nestjs project installation and a new controller quotes

---
+++ quotes-server/src/quotes/quotes.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('quotes')
+export class QuotesController {
+
+    @Get()
+    getQuotes(): string {
+        return "hello from quotes";
+    }
+}
