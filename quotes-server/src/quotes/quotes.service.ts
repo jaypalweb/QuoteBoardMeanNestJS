4af3d721f21f42aee97e0004987e0aa812c81fe8
Jay Pal <contactjaypal@gmail.com>
2020-05-01 23:58:46 +0530

    Making Post Request

--- quotes-server/src/quotes/quotes.service.ts
+++ quotes-server/src/quotes/quotes.service.ts
@@ -5,4 +5,8 @@ export class QuotesService {
     getQuotes(): string {
         return 'This will return all the quotes';
     }
+
+    createQuotes(quote: any): string {
+        return `${quote.author} has written "${quote.title}"`;
+    }
 }
