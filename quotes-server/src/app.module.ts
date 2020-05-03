8ff8b104996438d5a1b5c7f25df94b28a9fc976a
Jay Pal <contactjaypal@gmail.com>
2020-05-03 22:11:11 +0530

    Adding Quotes Module

--- quotes-server/src/app.module.ts
+++ quotes-server/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuotesController } from './quotes/quotes.controller';
-import { QuotesService } from './quotes/quotes.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { QuotesModule } from './quotes/quotes.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
-  controllers: [AppController, QuotesController],
-  providers: [AppService, QuotesService],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), QuotesModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
