0c49c7b9ab09275ce85f071c82ce117f355ce77d
Jay Pal <contactjaypal@gmail.com>
2020-05-06 01:15:00 +0530

    Catching Quote Not Found

--- quotes-server/src/quotes/quotes.service.ts
+++ quotes-server/src/quotes/quotes.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { Quote } from './interfaces/quote.interface';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
@@ -14,7 +14,11 @@ export class QuotesService {
     }
 
     async getQuote(id: string): Promise<Quote> {
-        return await this.quoteModel.findById(id).exec();
+        try {
+            return await this.quoteModel.findById(id).exec();
+        } catch (err) {
+            throw new HttpException('Quote Not Found', HttpStatus.NOT_FOUND);
+        }
     }
 
     async createQuotes(quote: Quote): Promise<Quote> {
