fdec97d4128edb4f41db691a0d90a70eda6fd702
Jay Pal <contactjaypal@gmail.com>
2020-05-07 11:21:36 +0530

    Adding Lazy Loading Module

--- quotes-client/src/app/app-routing.module.ts
+++ quotes-client/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'quotes', loadChildren: () => import('./quotes/quotes.module').then(m => m.QuotesModule) }
 ];
 
 @NgModule({
