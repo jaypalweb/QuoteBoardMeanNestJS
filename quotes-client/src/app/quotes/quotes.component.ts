faacc33848ada679643ae6d1a7236d3e249d66e7
Jay Pal <contactjaypal@gmail.com>
2020-05-08 22:01:22 +0530

    Adding Random Avatar Background

--- quotes-client/src/app/quotes/quotes.component.ts
+++ quotes-client/src/app/quotes/quotes.component.ts
@@ -12,4 +12,13 @@ export class QuotesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return { background: color };
+  }
+
 }
