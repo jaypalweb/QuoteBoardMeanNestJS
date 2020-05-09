bb71bda5f11e3ffb55924757e422df2eaa7bf550
Jay Pal <contactjaypal@gmail.com>
2020-05-09 19:46:43 +0530

    Adding Create Quote Component

--- quotes-client/src/app/app-routing.module.ts
+++ quotes-client/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'quotes', loadChildren: () => import('./quotes/quotes.module').then(m => m.QuotesModule) }
+  { path: 'quotes', loadChildren: () => import('./quotes/quotes.module').then(m => m.QuotesModule) },
+  { path: 'edit', loadChildren: () => import('./edit-quote/edit-quote.module').then(m => m.EditQuoteModule) }
 ];
 
 @NgModule({
